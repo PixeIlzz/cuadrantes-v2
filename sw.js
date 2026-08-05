94691bbc45371129dd29c4265b884b3c0d46bf30
PixeIlzz <franciscojavierleonperezz@gmail.com>
2026-08-05 16:36:32 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Service worker de Cuadrantes.
    Sube VERSION en cada despliegue: al cambiar, el navegador detecta el
    service worker nuevo, descarga los archivos y avisa al usuario. */
-const VERSION = 'v25';
+const VERSION = 'v26';
 const CACHE = 'cuadrantes-' + VERSION;
 
 const ARCHIVOS = [
